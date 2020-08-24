c1099a5a064be1ca385236ad1de9a9358bdd8be8
jaikishan15 <jai.kishan_cs19@gla.ac.in>
2020-08-24 13:48:43 +0530

    Initial commit

---
+++ src/dataTypes/TypesOfDatatypes.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Jai Kishan
+ *   Date: 24-08-2020
+ *   Time: 13:47
+ *   File: TypesOfDatatypes.java
+ */
+
+package dataTypes;
+
+public class TypesOfDatatypes {
+}
